3bea9ce1244943a683b801bb8dd3b1836b0984db
mhmdmaani <mohammeedmaani@hotmail.com>
2021-06-12 12:31:12 +0200

    fetch students to client

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import models.Student;
 
 import java.io.*;
 import java.net.Socket;
@@ -7,10 +8,12 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
+
 public class Client {
     static String contentType = "";
     static String url = "";
@@ -19,7 +22,7 @@ public class Client {
     static String fileExtension = "";
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
 
        String url ="";
@@ -27,64 +30,32 @@ public class Client {
         System.out.println("1. Fetch students");
         System.out.println("2. Fetch student by Id");
         System.out.println("3. insert a new student");
-
-        System.out.println("Choose a choice");
-        System.out.println("Choose a choice");
-       url = sc.nextLine();
-        HttpRequest getRequest = HttpRequest.newBuilder()
-                .GET()
-                .header("accept", contentType)
-                .uri(URI.create("http://localhost/" + url))
-                .build();
-        HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
-
-    }
-
-    private static void GetRequests(String url, String urlType) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest getRequest = HttpRequest.newBuilder()
-                .GET()
-                .header("accept", contentType)
-                .uri(URI.create("http://localhost/" + url))
-                .build();
-        HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
-
-        if (url.equals("storage")) {
-            HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
-            if (urlType.equals("HEAD")) {
-                System.out.println(response.headers());
-            } else {
-                ObjectMapper mapper = new ObjectMapper();
-                List<Person> posts = mapper.readValue(response.body(), new TypeReference<>() {
-                });
-                posts.forEach(System.out::println);
-            }
-        } else if (url.contains("?")) {
-            HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
-            if (urlType.equals("HEAD")) {
-                System.out.println(response.headers());
-            } else {
-                ObjectMapper mapper = new ObjectMapper();
-                Person post = mapper.readValue(response.body(), new TypeReference<>() {
-                });
-                System.out.println(post);
-            }
-
-        } else if (url.contains(".")) {
-            HttpResponse<byte[]> response = client.send(getRequest, HttpResponse.BodyHandlers.ofByteArray());
-            if (urlType.equals("HEAD")) {
-                System.out.println(response.headers());
-            } else {
-                if (200 == response.statusCode()) {
-                    byte[] bytes = response.body();
-                    try (OutputStream out = new FileOutputStream(url)) {
-                        out.write(bytes);
-                    }
-                }
-            }
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                printStudents(client);
+            default:
+                System.out.println("enter");
         }
-    }
 
+
+       url = sc.nextLine();
+
+
+    }
+    public static void printStudents(HttpClient client) throws IOException, InterruptedException {
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "text/json")
+                .uri(URI.create("http://localhost:5050/students"))
+                .build();
+        HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Student> students = mapper.readValue(response.body(), new TypeReference<>() {
+        });
+       students.forEach(student->System.out.println(student));
+    }
     public static CompletableFuture<Void> POSTRequest(Map<String, String> bodyText) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         String requestBody = objectMapper
